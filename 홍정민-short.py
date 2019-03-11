1f028cb4635654287bfc5a60843833f965f8dc0a
passingbreeze <jungmin1237@pusan.ac.kr>
2019-03-12 01:40:59 +0900

    solving...

--- 홍정민-short.py
+++ 홍정민-short.py
@@ -1,7 +1,7 @@
-import networkx as nx, matplotlib.pyplot as plt, numpy as np
+import networkx as nx, matplotlib.pyplot as plt, numpy as np, itertools as its
 
 def isEdge(a, b, L):
-    return a!=b and np.sqrt((np.power(a[0]-b[0],2)+np.power(a[1]-b[1],2)))<=L
+    return (np.sqrt((np.power(a[0]-b[0],2)+np.power(a[1]-b[1],2)))<=L)
 
 def main():
     g = nx.Graph()
@@ -19,18 +19,16 @@ def main():
 
     for i in range(1,len(nodeList)+1):
         for j in range(1,len(nodeList)+1):
-            if isEdge(nodeList[i], nodeList[j], length):
-                # print(nodeList[i], nodeList[j])
+            if nodeList[i]!=nodeList[j] and isEdge(nodeList[i], nodeList[j], length):
                 g.add_edge(i,j)
-    # print("shortest path : ", nx.shortest_path(g, source=s, target=t))
 
+    print("shortest path : ", nx.shortest_path(g, source=s, target=t))
     nx.draw_networkx_nodes(g, nodeList, node_size=5)  # 그리려는 그래프의 속성을 설정해 준다
     nx.draw_networkx_edges(g, nodeList, width=1)
     nx.draw_networkx_labels(g, nodeList, font_size=10, font_family='sans-serif')
 
     plt.axis('on')  # 그래프 그림을 파일로 저장한다
-    plt.savefig("graph.png", dpi=1000)  # 그림 파일 크기 지정
-    plt.savefig("홍정민_graph.png")  # png 파일로 저장
+    plt.savefig("홍정민_graph.png", dpi=1000)  # 그림 파일 크기 지정
     plt.show()
     return
 
